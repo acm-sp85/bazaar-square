67b212752afa75e7ba38a115a0dc96d5a5296653
Alex-new-laptop <acontell@gmail.com>
2021-11-23 18:40:31 -0500

    cleaning up React template

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch("/items", {
+      credentials: "include",
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((user) => {
+          console.log(user);
+        });
+      } else {
+        console.log("error");
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
+    // BEM convention
+    <div className="app">
       <h1>Bazaar Square</h1>
     </div>
   );
