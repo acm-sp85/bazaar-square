29eea87268765ae136a35ef35c2f0926a2f38e7c
Alex-new-laptop <acontell@gmail.com>
2021-12-01 09:02:10 -0500

    Front end form to log in

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   useEffect(() => {
     fetch("/items", {
       credentials: "include",
@@ -17,36 +19,63 @@ function App() {
   }, []);
 
   // TEMPORARY FAKE-LOGING
-  const login = (e) => {
+  // const login = (e) => {
+  //   e.preventDefault();
+
+  //   const requestOptions = {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       // user_name: email,
+  //       // password: password,
+  //     }),
+  //   };
+
+  //   fetch("/login", requestOptions).then((response) => {
+  //     if (response.ok) {
+  //       response.json().then((user) => {
+  //         console.log(user);
+  //       });
+  //     } else {
+  //       response.json().then((error) => {
+  //         console.log(error);
+  //       });
+  //     }
+  //   });
+
+  //   fetch("/me").then((response) => {
+  //     response.json().then((x) => {
+  //       console.log(x);
+  //     });
+  //   });
+  // };
+  const handleSubmit = (e) => {
     e.preventDefault();
-
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        // user_name: email,
-        // password: password,
-      }),
-    };
-
-    fetch("/login", requestOptions).then((response) => {
-      if (response.ok) {
-        response.json().then((user) => {
-          console.log(user);
-        });
-      } else {
-        response.json().then((error) => {
-          console.log(error);
-        });
-      }
-    });
+    console.log(email);
   };
 
   return (
     // BEM convention
     <div className="app">
       <h1>Bazaar Square</h1>
-      <button onClick={login}>LogIn</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="custom-imputs"
+          type="email"
+          placeholder="Email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="custom-imputs"
+          type="password"
+          placeholder="Password..."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>LOGIN</button>
+      </form>
+      {/* <button onClick={login}>LogIn</button> */}
     </div>
   );
 }
