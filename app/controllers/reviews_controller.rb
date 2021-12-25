234c9e4af86355f749798d13658ff444753218ef
Alex-new-laptop <acontell@gmail.com>
2021-12-25 18:54:04 +0100

    modified Review model to have Optional associations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,63 @@
 class ReviewsController < ApplicationController
+    before_action :check_authorization, except: [:create, :index]
+    before_action :set_review, only: [:show, :destroy, :update]
 
     def index
         reviews = Review.all 
          render json: reviews, each_serializer: ReviewSerializer
 
     end
+
+    def show
+    if @review 
+      render json:  @review, serializer: ReviewSerializer, status: :ok
+    else
+      render json: {error: "Review not found"} , status: :not_found
+    end
+    end
+
+    def create
+    review = Review.create(reviews_params)
+    if review.valid?
+      
+      render json: review, status: :created
+
+    else
+      render json: {errors: review.errors.full_messages}, status: :unprocessable_entity 
+    end
+    end
+
+    def update
+      updated_review = @review.update(reviews_params)
+      if updated_review
+        render json: @review, status: :accepted
+      else
+        render json: {error: "Impossible to update review"}, status: :unprocessable_entity 
+      end
+      
+    end  
+
+    def destroy
+      
+    if @review.destroy
+
+      head :no_content, status: :ok
+
+    else
+      ender json: {error: "Impossible to delete review"}, status: :unprocessable_entity 
+    end
+    end
     
+
+
+
+      private
+
+  def reviews_params
+        params.permit(:id, :item_id, :user_id, :review_content , :review_rating, :review_type , :review_title, :author_id)
+  end
+
+  def set_review
+        @review = Review.find_by(id: params[:id])
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
      belongs_to :user_reviewer, class_name: 'User', foreign_key: 'author_id'
-     belongs_to :user_reviewed, class_name: 'User', foreign_key: 'user_id'
-     belongs_to :item 
+     belongs_to :user_reviewed, class_name: 'User', foreign_key: 'user_id', optional: true
+     belongs_to :item , optional: true
 
 end
  
