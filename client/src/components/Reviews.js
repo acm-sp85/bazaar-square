35f533bd491ba404466fbc9b0ef8a2f66f162125
Alex-new-laptop <acontell@gmail.com>
2021-12-28 16:59:36 +0100

    added functionality inside of Users reviews pannel

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,9 +1,13 @@
 import React from "react";
+import ReviewsPannel from "./ReviewsPannel";
 
-function Reviews() {
+function Reviews({ currentUser }) {
   return (
     <div>
-      <h1>REVIEWS</h1>
+      <h1>{currentUser.user_name}'s reviews</h1>
+      {currentUser.reviews.map((review) => (
+        <ReviewsPannel review={review} key={review.id} />
+      ))}
     </div>
   );
 }
