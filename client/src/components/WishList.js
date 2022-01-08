53ac677c8982f15ccbb8600ab1330fb7dbb0eb6e
Alex-new-laptop <acontell@gmail.com>
2022-01-08 17:42:22 +0100

    wishlist updated, just missing item exclusivity

--- client/src/components/WishList.js
+++ client/src/components/WishList.js
@@ -1,23 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Wish from "./Wish";
 
 export default function WishList(props) {
-  const [wished_items, setWished_items] = useState(props.currentUser.wishlists);
+  const [wished_items, setWished_items] = useState(null);
+
+  useEffect(() => {
+    setWished_items(props.currentUser.wishlists);
+  }, [props]);
 
   return (
     <div>
       <h1>WISHLIST</h1>
-      <div className="wishlist__grid">
-        {wished_items.map((item) => (
-          <Wish
-            info={item}
-            setWished_items={setWished_items}
-            wished_items={wished_items}
-            key={item.id}
-          />
-        ))}
-      </div>
+      {wished_items ? (
+        <div className="wishlist__grid">
+          {wished_items.map((item) => (
+            <Wish
+              info={item}
+              setWished_items={setWished_items}
+              wished_items={wished_items}
+              key={item.id}
+            />
+          ))}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
