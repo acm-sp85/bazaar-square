d814b945b8926b89776094ad7c5c6452f1851e2f
Alex-new-laptop <acontell@gmail.com>
2022-06-15 15:21:28 -0400

    Giving access to Homepage for not logged in users

--- client/src/UnauthenticatedApp.js
+++ client/src/UnauthenticatedApp.js
@@ -1,37 +1,37 @@
-import React from "react";
-import "./styles/Home.css";
-import { useHistory } from "react-router-dom";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
+import React from 'react';
+import './styles/Home.css';
+import { useHistory } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
-import Reviews from "./components/Reviews";
-import SearchResults from "./components/SearchResults";
+import Reviews from './components/Reviews';
+import SearchResults from './components/SearchResults';
 
-import CategoryPage from "./components/CategoryPage";
-import LocationPage from "./components/LocationPage";
-import UserProfilePublic from "./components/UserProfilePublic";
-import ItemInfo from "./components/ItemInfo";
-import Home from "./components/Home";
-import Header from "./components/Header";
+import CategoryPage from './components/CategoryPage';
+import LocationPage from './components/LocationPage';
+import UserProfilePublic from './components/UserProfilePublic';
+import ItemInfo from './components/ItemInfo';
+import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-import ItemTypeCategory from "./components/ItemTypeCategory";
+import ItemTypeCategory from './components/ItemTypeCategory';
 
 function UnauthenticatedApp({ setCurrentUser, categories, cities }) {
   const history = useHistory();
   return (
     <div>
-    
       <BrowserRouter>
-        {/* <Header /> */}
+        <Header />
         <Switch>
-          <Route exact path="/">
-            <Login setCurrentUser={setCurrentUser} />
-          </Route>
           <Route exact path="/signup">
             <Signup setCurrentUser={setCurrentUser} />
           </Route>
-          {/* <Route
+          <Route exact path="/login">
+            <Login setCurrentUser={setCurrentUser} />
+          </Route>
+          <Route
             exact
             path="/"
             render={(props) => (
@@ -41,20 +41,10 @@ function UnauthenticatedApp({ setCurrentUser, categories, cities }) {
 
           <Route
             exact
-            path="/reviews"
-            render={(props) => <Reviews props={props} />}
-          />
-          <Route
-            exact
             path="/search"
             render={(props) => <SearchResults props={props} />}
           />
 
-          <Route
-            exact
-            path="/location"
-            render={(props) => <LocationPage props={props} />}
-          />
           <Route
             exact
             path="/category"
@@ -75,10 +65,11 @@ function UnauthenticatedApp({ setCurrentUser, categories, cities }) {
             exact
             path="/item-type"
             render={(props) => <ItemTypeCategory props={props} />}
-          /> */}
+          />
 
           <Redirect to="/" />
         </Switch>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React from 'react';
 
-import LeftComponent from "./LeftComponent";
-import RightComponent from "./RightComponent";
-import ItemTypesCarousel from "./ItemTypesCarousel";
-import CategoriesCarousel from "./CategoriesCarousel";
-import AreasCarousel from "./AreasCarousel";
+import LeftComponent from './LeftComponent';
+import RightComponent from './RightComponent';
+import ItemTypesCarousel from './ItemTypesCarousel';
+import CategoriesCarousel from './CategoriesCarousel';
+import AreasCarousel from './AreasCarousel';
 
 function Home({ currentUser, setCurrentUser, categories, cities }) {
   return (
     <div className="home">
-      <h1 style={{ textAlign: "center" }}>
-        Welcome {currentUser.user_name}!
-      </h1>
-      <h3 variant="body1" style={{ textAlign: "center", color: "grey" }}>
+      {currentUser ? (
+        <h1 style={{ textAlign: 'center' }}>
+          Welcome {currentUser.user_name}!{' '}
+        </h1>
+      ) : (
+        <></>
+      )}
+
+      <h3 variant="body1" style={{ textAlign: 'center', color: 'grey' }}>
         Random Bazaar is a market place to share goods with your community.
       </h3>
-      <h4 variant="body1" style={{ textAlign: "center", color: "grey" }}>
+      <h4 variant="body1" style={{ textAlign: 'center', color: 'grey' }}>
         Donate, borrow, trade or sell your goods in our platform.
       </h4>
 
